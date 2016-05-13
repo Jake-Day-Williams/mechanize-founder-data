372e8daff090704c9d569bd72d3c72782ec4eee2
Jake Day Williams <jake@wildacademy.com>
2016-05-13 17:22:44 +0000

    Captured name,birthday,deathday of listed.

--- mechanize.rb
+++ mechanize.rb
@@ -1,32 +1,28 @@
 require 'rubygems'
 require 'mechanize'
 
-#search for these items on Wikipedia
-search_terms = ['steve jobs', 'elon musk', 'larry page', 'mark zuckerburg']
+# This program will search for the Wikipedia page listings for the individuals listed 
+# in the search_terms array and return their full name, birth date, and day of death.
+search_terms = ['george washington', 'benjamin franklin', 'thomas jefferson', 'john adams']
 
-#setup Mechanize
-agent= Mechanize.new { |agent|
-  agent.user_agent_alias = 'Windows Chrome'
+# Setup Mechanize
+agent= Mechanize.new { |a|
+  a.user_agent_alias = 'Windows Chrome'
 }
 
-#setup the page
+# Setup the page. This will select the search box on Wikidedia.
 page = agent.get('https://www.wikipedia.org/')
 search_form = page.form(:action => '//www.wikipedia.org/search-redirect.php')
 
+# For each item in the search_terms array do these things
 search_terms.each do |term|
   search_form.search = term
   results = agent.submit(search_form, search_form.button('go'))
   html_results = Nokogiri::HTML(results.body)
   name = html_results.at_css('#firstHeading').text
   bday = html_results.at_css('.bday').text
-  puts name + " was born on " + bday
+  dday = html_results.at_css('.dday').text
+  puts name + " was born on " + bday + " and died on " + dday
 end
 
-
-
-
-# name = html_results.at_css('#firstHeading').text
-# bday = html_results.at_css('.bday').text
-# dday = html_results.at_css('.dday').text
-
-# puts name, bday, dday
+# Now run the file in the terminal. $ ruby mechanize.rb
